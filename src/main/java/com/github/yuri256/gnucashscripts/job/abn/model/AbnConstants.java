4e0f50c683a2510220b45cc20d2dc4cfd85830d2
yuri256 <yuri256@github.com>
2020-12-23 22:09:18 +0100

    ABN: remove "BIC", "MARF", "EREF", "CSID", "IBAN" fields from import. remove words "REMI", "NAME", "TRTP" from import.

--- src/main/java/com/github/yuri256/gnucashscripts/job/abn/model/AbnConstants.java
+++ src/main/java/com/github/yuri256/gnucashscripts/job/abn/model/AbnConstants.java
@@ -4,7 +4,7 @@ import java.util.Collections;
 import java.util.Set;
 
 public class AbnConstants {
-    public static final Set<String> DEFAULT_REMOVE_FIELD_KEYS = Collections.unmodifiableSet(Set.of("BIC"));
-    public static final Set<String> DEFAULT_REMOVE_KEY_KEYS = Collections.unmodifiableSet(Set.of());
+    public static final Set<String> DEFAULT_REMOVE_FIELD_KEYS = Collections.unmodifiableSet(Set.of("BIC", "MARF", "EREF", "CSID", "IBAN"));
+    public static final Set<String> DEFAULT_REMOVE_KEY_KEYS = Collections.unmodifiableSet(Set.of("REMI", "NAME", "TRTP"));
     public static final Set<String> KNOWN_FIELDS_WITH_SPACE = Collections.unmodifiableSet(Set.of());
 }
