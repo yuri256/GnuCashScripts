46316edd87b48cbf35187accf79ec4b6d5d81aea
yuri256 <yuri256@github.com>
2024-06-03 00:00:41 +0200

    ING: change file name:
    - Add 'ing_' prefix
    - Change date to ISO format: yyyy-mm-dd

--- src/main/java/com/github/yuri256/gnucashscripts/cli/action/Move.java
+++ src/main/java/com/github/yuri256/gnucashscripts/cli/action/Move.java
@@ -10,12 +10,17 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @CommandLine.Command(name = "move", description = "Move files from 'download' directory to 'in' directory of appropriate processor")
 public class Move implements Runnable {
+
+    private static final Pattern ING_FILENAME_PATTERN = Pattern.compile("^NL\\d{2}INGB\\d{10}_(\\d{2}-\\d{2}-\\d{4})_(\\d{2}-\\d{2}-\\d{4})\\.csv$");
+
     @Override
     public void run() {
-                String downloadsDir = Config.load().get(Property.DOWNLOADS_DIR);
+        String downloadsDir = Config.load().get(Property.DOWNLOADS_DIR);
         if (downloadsDir == null) {
             System.out.println("downloadsDir not set");
             System.exit(1);
@@ -42,19 +47,21 @@ public class Move implements Runnable {
 
         try {
             Files.list(downloadsDirFile.toPath()).forEach(path -> {
-                if (path.getFileName().toString().startsWith("NL03INGB")
-                        && path.getFileName().toString().endsWith(".csv")) {
-                    System.out.println("ing = " + path);
-
+                String fileName = path.getFileName().toString();
+                Matcher matcher = ING_FILENAME_PATTERN.matcher(fileName);
+                if (matcher.matches()) {
                     try {
-                        Files.move(path, ingInDir.resolve(path.getFileName()));
+                        String targetFileName = "ing_" + fileName.substring(0, 18) + "_" + flipDateIng(matcher.group(1)) + "_" + flipDateIng(matcher.group(2)) + ".csv";
+                        Path targetPath = ingInDir.resolve(targetFileName);
+                        System.out.println("Moving " + path + " to " + targetPath);
+                        Files.move(path, targetPath);
                     } catch (IOException e) {
                         System.out.println("Could not move file " + path + " to " + ingInDir + ": " + e);
                     }
                 }
 
-                if (path.getFileName().toString().startsWith("MT940")
-                        && path.getFileName().toString().endsWith(".STA")) {
+                if (fileName.startsWith("MT940")
+                        && fileName.endsWith(".STA")) {
                     System.out.println("abn = " + path);
                     try {
                         Files.move(path, abnInDir.resolve(path.getFileName()));
@@ -66,5 +73,10 @@ public class Move implements Runnable {
         } catch (IOException e) {
             System.out.println("Error: " + e.getMessage());
         }
+    }
+
+    private String flipDateIng(String dateString) {
+        String[] parts = dateString.split("-");
+        return parts[2] + "-" + parts[1] + "-" + parts[0];
     }
 }
